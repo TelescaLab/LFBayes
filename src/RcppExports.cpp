c1ffb316c953ba1340487a074a8cb2c30794c760
John Shamshoian <jshamsho@gmail.com>
2018-11-14 18:29:50 -0800

    Returning fewer items for lower memory cost

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -67,49 +67,6 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _LFBayes_rcpparma_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _LFBayes_rcpparma_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _LFBayes_rcpparma_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _LFBayes_rcpparma_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
 // convertToPrecision
 arma::mat convertToPrecision(arma::vec Delta, arma::uword q1, arma::uword q2);
 RcppExport SEXP _LFBayes_convertToPrecision(SEXP DeltaSEXP, SEXP q1SEXP, SEXP q2SEXP) {
@@ -601,10 +558,6 @@ static const R_CallMethodDef CallEntries[] = {
     {"_LFBayes_getMarginalFunc", (DL_FUNC) &_LFBayes_getMarginalFunc, 3},
     {"_LFBayes_getMarginalLong", (DL_FUNC) &_LFBayes_getMarginalLong, 3},
     {"_LFBayes_mcmcWeak", (DL_FUNC) &_LFBayes_mcmcWeak, 10},
-    {"_LFBayes_rcpparma_hello_world", (DL_FUNC) &_LFBayes_rcpparma_hello_world, 0},
-    {"_LFBayes_rcpparma_outerproduct", (DL_FUNC) &_LFBayes_rcpparma_outerproduct, 1},
-    {"_LFBayes_rcpparma_innerproduct", (DL_FUNC) &_LFBayes_rcpparma_innerproduct, 1},
-    {"_LFBayes_rcpparma_bothproducts", (DL_FUNC) &_LFBayes_rcpparma_bothproducts, 1},
     {"_LFBayes_convertToPrecision", (DL_FUNC) &_LFBayes_convertToPrecision, 3},
     {"_LFBayes_updateBeta2", (DL_FUNC) &_LFBayes_updateBeta2, 3},
     {"_LFBayes_updateBetaProd", (DL_FUNC) &_LFBayes_updateBetaProd, 5},
